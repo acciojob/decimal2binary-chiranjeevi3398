c737aa548cead7d4703bc386638322302f22792b
chiranjeevi gurrapu <117752857+chiranjeevi3398@users.noreply.github.com>
2023-07-03 17:01:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
 
-module.exports = threeSum;
+  function decimalToBinary(decimal) {
+  return decimal.toString(2);
+} 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)) 
